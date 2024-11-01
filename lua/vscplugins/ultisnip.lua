0f4595331c913a94e324582006573af7f77716fd
Yueyang Li <egoistheresy@outlook.com>
2024-11-01 00:07:13 -0400

    Updating the nvim lua settings
    Daily update, love my little fish

---
+++ lua/vscplugins/ultisnip.lua
@@ -0,0 +1,9 @@
+return {"SirVer/ultisnips",
+    lazy = false, 
+    config = 
+    function()
+        vim.g.UltiSnipsExpandOrJumpTrigger = '<tab>'
+        vim.g.UltiSnipsJumpBackwardTrigger = '<s-tab>'
+        vim.g.UltiSnipsSnippetDirectories = {"./UltiSnips"}
+    end
+}

01de395444b2b7e9707d2141c70cfa3032b34fd6
Yueyang Li <egoistheresy@outlook.com>
2025-02-22 17:48:22 -0500

    changed part of the file to let lazyVim to work with powershell

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,20 +1,6 @@
 vim.g.mapleader = ","
 vim.g.maplocalleader = "\\"
 
-vim.opt.backspace = "2"
-vim.opt.showcmd = true
-vim.opt.laststatus = 2
-vim.opt.autowrite = true
-vim.opt.cursorline = true
-vim.opt.autoread = true
-vim.opt.relativenumber = true --enable absolute line numbers
-
--- use spaces for tabs and whatnot
-vim.opt.tabstop = 2 -- Set the display of Tab-Size to be 4 ('/t' Character will be displayed as 4 spaces)
-vim.opt.shiftwidth = 2 -- Indent with 4 spaces
-vim.opt.shiftround = true -- This will round the indentation to the nearest "Multiples" of "Shiftwidth" 7 -> 8 if pressing "Tab"
-vim.opt.expandtab = true -- uses spaces instaed of tabs (Will be replacing the '\t' character with " " characters)
-
 -- keymappings
 -- normal mode settings
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>") -- setting "," + "h" to cancel the highlight
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,40 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- [[
+-- Setting Line numebrs relatedd staff.
+-- ]]
+vim.opt.number = true
+vim.opt.relativenumber = true --enable absolute line numbers
+vim.opt.statuscolumn = "%s %4l|%3r  " -- set with absolute and relative number at the same time
+
+-- Set the absolute and relative number colors
+vim.api.nvim_set_hl(0, "StatusColumnAbsolute", { fg = "#ffcc66", bold = true }) -- Yellow absolute numbers
+vim.api.nvim_set_hl(0, "StatusColumnRelative", { fg = "#99cc99" }) -- Green relative numbers
+vim.api.nvim_set_hl(0, "StatusColumnSeparator", { fg = "#666666" }) -- Gray separator
+vim.o.statuscolumn = "%#StatusColumnAbsolute# %4l%#StatusColumnSeparator#│%#StatusColumnRelative#%3r  "
+
+-- use spaces for tabs and whatnot
+vim.opt.tabstop = 2 -- Set the display of Tab-Size to be 4 ('/t' Character will be displayed as 4 spaces)
+vim.opt.shiftwidth = 2 -- Indent with 4 spaces
+vim.opt.shiftround = true -- This will round the indentation to the nearest "Multiples" of "Shiftwidth" 7 -> 8 if pressing "Tab"
+vim.opt.expandtab = true -- uses spaces instaed of tabs (Will be replacing the '\t' character with " " characters)
+vim.opt.backspace = "2"
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+--[[
+-- Settings the Default Shell
+--]]
+--Set the default shell program
+vim.opt.shell = "powershell.exe"
+-- Adjust quoting
+vim.opt.shellxquote = ""
+vim.opt.shellquote = ""
+--  Set the redirection scheme and encoding
+vim.opt.shellredir = "2>&1 | Out-File -Encoding UTF8"
+vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8"

cc52c54479432ce30aeaebeb0cce5f8b10688eb4
Yueyang Li <egoistheresy@outlook.com>
2025-05-01 17:59:43 -0400

    updated

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
+-- Or for a more visible notification:
+vim.notify("options.lua module loaded successfully", vim.log.levels.INFO)
+
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
@@ -5,15 +8,14 @@
 -- [[
 -- Setting Line numebrs relatedd staff.
 -- ]]
-vim.opt.number = true
-vim.opt.relativenumber = true --enable absolute line numbers
-vim.opt.statuscolumn = "%s %4l|%3r  " -- set with absolute and relative number at the same time
+vim.o.number = true
+vim.o.relativenumber = true --enable absolute line numbers
 
 -- Set the absolute and relative number colors
 vim.api.nvim_set_hl(0, "StatusColumnAbsolute", { fg = "#ffcc66", bold = true }) -- Yellow absolute numbers
 vim.api.nvim_set_hl(0, "StatusColumnRelative", { fg = "#99cc99" }) -- Green relative numbers
 vim.api.nvim_set_hl(0, "StatusColumnSeparator", { fg = "#666666" }) -- Gray separator
-vim.o.statuscolumn = "%#StatusColumnAbsolute# %4l%#StatusColumnSeparator#│%#StatusColumnRelative#%3r  "
+vim.o.statuscolumn = "%#StatusColumnAbsolute# %4l%#StatusColumnSeparator#│%#StatusColumnRelative#%3r"
 
 --------------------------------------------------------------------------------------------------------
 
@@ -24,9 +26,8 @@ vim.opt.tabstop = 4 -- Set the display of Tab-Size to be 4 ('/t' Character will 
 vim.opt.shiftwidth = 4 -- Indent with 4 spaces
 vim.opt.shiftround = true -- This will round the indentation to the nearest "Multiples" of "Shiftwidth" 7 -> 8 if pressing "Tab"
 vim.opt.expandtab = true -- uses spaces instaed of tabs (Will be replacing the '\t' character with " " characters)
-vim.opt.backspace = "2"
 vim.opt.showcmd = true
-vim.opt.laststatus = 4
+vim.opt.laststatus = 2
 vim.opt.autowrite = true
 vim.opt.cursorline = true
 vim.opt.autoread = true

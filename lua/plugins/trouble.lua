2663e0a3961cdeba6e405f9b2197177bf1e2ada9
Yueyang Li <egoistheresy@outlook.com>
2025-06-08 04:35:22 -0400

    fixed the error location issue

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,11 @@
+-- setting up some trouble notifications
+
+return
+{
+    "folke/trouble.nvim",
+    opts = {
+        position = "bottom", -- Move the window to bottom of the screen
+        height = 10,
+        width = 60,
+    }
+}
